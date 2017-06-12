5cceec25c272c9a4bdece34ac46f1c2f7ff8d61b
ReynardoEW <reynardoew@gmail.com>
2017-06-12 19:27:43 +0700

    added full function

--- TWRP Backupper/Form1.cs
+++ TWRP Backupper/Form1.cs
@@ -79,7 +79,24 @@ namespace TWRP_Backupper
                     }
                     else
                     {
-                       
+                        var proc = new Process
+                        {
+                            StartInfo = new ProcessStartInfo
+                            {
+                                FileName = "adb.exe",
+                                Arguments = "backup " + saveFile + "--twrp system cache data boot",
+                                UseShellExecute = false,
+                                RedirectStandardOutput = true,
+                                CreateNoWindow = false
+                            }
+                        };
+                        proc.Start();
+                        while (!proc.StandardOutput.EndOfStream)
+                        {
+                            string line = proc.StandardOutput.ReadLine();
+                            Console.WriteLine(line);
+
+                        }
                     }
                     Console.WriteLine(saveFile);
 
@@ -120,6 +137,248 @@ namespace TWRP_Backupper
             else
             {
                
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            saveFileDialog1.Filter = "ZIP|*.zip|RAR|*.rar";
+
+            saveFileDialog1.Title = "Save Backup";
+            DialogResult result = saveFileDialog1.ShowDialog();
+
+
+            if (result == DialogResult.OK)
+            {
+
+                string saveFile = saveFileDialog1.FileName;
+                try
+                {
+                    if (MessageBox.Show("Do you wanna compress?", "Confirm", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+                    {
+                        var proc = new Process
+                        {
+                            StartInfo = new ProcessStartInfo
+                            {
+                                FileName = "adb.exe",
+                                Arguments = "backup " + saveFile + "--twrp data --compress",
+                                UseShellExecute = false,
+                                RedirectStandardOutput = true,
+                                CreateNoWindow = false
+                            }
+                        };
+                        proc.Start();
+                        while (!proc.StandardOutput.EndOfStream)
+                        {
+                            string line = proc.StandardOutput.ReadLine();
+                            Console.WriteLine(line);
+
+                        }
+                    }
+                    else
+                    {
+                        var proc = new Process
+                        {
+                            StartInfo = new ProcessStartInfo
+                            {
+                                FileName = "adb.exe",
+                                Arguments = "backup " + saveFile + "--twrp data",
+                                UseShellExecute = false,
+                                RedirectStandardOutput = true,
+                                CreateNoWindow = false
+                            }
+                        };
+                        proc.Start();
+                        while (!proc.StandardOutput.EndOfStream)
+                        {
+                            string line = proc.StandardOutput.ReadLine();
+                            Console.WriteLine(line);
+
+                        }
+                    }
+                    Console.WriteLine(saveFile);
+
+
+
+                }
+                catch (IOException)
+                {
+                }
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            saveFileDialog1.Filter = "ZIP|*.zip|RAR|*.rar";
+
+            saveFileDialog1.Title = "Save Backup";
+            DialogResult result = saveFileDialog1.ShowDialog();
+
+
+            if (result == DialogResult.OK)
+            {
+
+                string saveFile = saveFileDialog1.FileName;
+                try
+                {
+                    if (MessageBox.Show("Do you wanna compress?", "Confirm", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+                    {
+                        var proc = new Process
+                        {
+                            StartInfo = new ProcessStartInfo
+                            {
+                                FileName = "adb.exe",
+                                Arguments = "backup " + saveFile + "--twrp system --compress",
+                                UseShellExecute = false,
+                                RedirectStandardOutput = true,
+                                CreateNoWindow = false
+                            }
+                        };
+                        proc.Start();
+                        while (!proc.StandardOutput.EndOfStream)
+                        {
+                            string line = proc.StandardOutput.ReadLine();
+                            Console.WriteLine(line);
+
+                        }
+                    }
+                    else
+                    {
+                        var proc = new Process
+                        {
+                            StartInfo = new ProcessStartInfo
+                            {
+                                FileName = "adb.exe",
+                                Arguments = "backup " + saveFile + "--twrp system",
+                                UseShellExecute = false,
+                                RedirectStandardOutput = true,
+                                CreateNoWindow = false
+                            }
+                        };
+                        proc.Start();
+                        while (!proc.StandardOutput.EndOfStream)
+                        {
+                            string line = proc.StandardOutput.ReadLine();
+                            Console.WriteLine(line);
+
+                        }
+                    }
+                    Console.WriteLine(saveFile);
+
+
+
+                }
+                catch (IOException)
+                {
+                }
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            saveFileDialog1.Filter = "ZIP|*.zip|RAR|*.rar";
+
+            saveFileDialog1.Title = "Save Backup";
+            DialogResult result = saveFileDialog1.ShowDialog();
+
+
+            if (result == DialogResult.OK)
+            {
+
+                string saveFile = saveFileDialog1.FileName;
+                try
+                {
+                    if (MessageBox.Show("Do you wanna compress?", "Confirm", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+                    {
+                        var proc = new Process
+                        {
+                            StartInfo = new ProcessStartInfo
+                            {
+                                FileName = "adb.exe",
+                                Arguments = "backup " + saveFile + "--twrp boot --compress",
+                                UseShellExecute = false,
+                                RedirectStandardOutput = true,
+                                CreateNoWindow = false
+                            }
+                        };
+                        proc.Start();
+                        while (!proc.StandardOutput.EndOfStream)
+                        {
+                            string line = proc.StandardOutput.ReadLine();
+                            Console.WriteLine(line);
+
+                        }
+                    }
+                    else
+                    {
+                        var proc = new Process
+                        {
+                            StartInfo = new ProcessStartInfo
+                            {
+                                FileName = "adb.exe",
+                                Arguments = "backup " + saveFile + "--twrp boot",
+                                UseShellExecute = false,
+                                RedirectStandardOutput = true,
+                                CreateNoWindow = false
+                            }
+                        };
+                        proc.Start();
+                        while (!proc.StandardOutput.EndOfStream)
+                        {
+                            string line = proc.StandardOutput.ReadLine();
+                            Console.WriteLine(line);
+
+                        }
+                    }
+                    Console.WriteLine(saveFile);
+
+
+
+                }
+                catch (IOException)
+                {
+                }
+            }
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            saveFileDialog1.Filter = "ZIP|*.zip|RAR|*.rar";
+
+            saveFileDialog1.Title = "Save Backup";
+            DialogResult result = saveFileDialog1.ShowDialog();
+
+
+            if (result == DialogResult.OK)
+            {
+
+                string saveFile = saveFileDialog1.FileName;
+              
+              
+                        var proc = new Process
+                        {
+                            StartInfo = new ProcessStartInfo
+                            {
+                                FileName = "adb.exe",
+                                Arguments = "restore " + saveFile,
+                                UseShellExecute = false,
+                                RedirectStandardOutput = true,
+                                CreateNoWindow = false
+                            }
+                        };
+                        proc.Start();
+                        while (!proc.StandardOutput.EndOfStream)
+                        {
+                            string line = proc.StandardOutput.ReadLine();
+                            Console.WriteLine(line);
+
+                        }
+                    
+                   
+
+
+                
+              
             }
         }
     }
